6388398db51e83ad3252d4c53b150be101c2ee87
nilSinger <niilz@outlook.de>
2018-09-28 23:16:01 +0200

    add the last code-alongs to the tutorial, although i still need to figure out why it behaved differently on my system (from 3d - 3g)

---
+++ part3_server_comparison/webserver03d.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+
+# Concurrent Server that does not close the child-process
+
+import os
+import socket
+
+SERVER_ADRESS = (HOST, PORT) = '', 8888
+REQUEST_QUEUE_SIZE = 5
+
+def handle_request(client_connection):
+    request = client_connection.recv(1024)
+    http_response = b"""\
+    HTTP/1.1 200 Ok
+
+    Hello, World!
+    """
+    client_connection.sendall(http_response)
+
+def serve_forever():
+    listen_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    listen_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    listen_socket.bind(SERVER_ADRESS)
+    listen_socket.listen(REQUEST_QUEUE_SIZE)
+    print(f'Servin HTTP on port {PORT}')
+
+    clients = []
+    while True:
+        client_connection, client_address = listen_socket.accept()
+        # store the reference otherwise it's garbage collected
+        # on the next loop run
+        clients.append(client_connection)
+        pid = os.fork()
+        if pid == 0: # child
+            listen_socket.close()
+            handle_request(client_connection)
+            client_connection.close()
+            os._exit(0) # child exits here
+        else: # parent
+            # client_connection.close()
+            print(len(clients))
+
+if __name__ == "__main__":
+    serve_forever()
